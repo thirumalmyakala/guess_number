19d1f68036d0e03c9e48c005afd508d549bf97e0
thirumalmyakala <thirumalmyakala@gmail.com>
2024-08-08 17:39:26 +0200

    code is working

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-Number = input("enter a number")
+import random
+
+def guess(x):
+    random_number=random.randint(1,x)
+    guess=0
+    while guess!=random_number:
+        guess=int(input(f"guess the number between 1 and {x} "))
+        if guess < random_number:
+            print("too low, guess again")
+        elif guess > random_number:
+            print("too high, try again")
+    print(f"yes, the guess number is {random_number}")
+
+guess(20)
